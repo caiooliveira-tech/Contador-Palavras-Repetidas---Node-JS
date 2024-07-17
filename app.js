c320809f0d2583a6530f0786e1854d69fcf887f7
Caio Oliveira <caio.oliveira07@gmail.com>
2024-07-17 20:51:49 -0300

    Implementada lógica de contagem de palavras

--- app.js
+++ app.js
@@ -7,6 +7,36 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 //lendo o arquivo e exibindo o conteúdo no console
 fs.readFile(link,'utf-8', (err, texto) => {
-    console.log(texto);
+    quebraEmParagrafos(texto);
+    // verificarPalavrasDuplicadas(texto);
 });
-console.log(link)
+
+//função que verifica palavras duplicadas
+function verificarPalavrasDuplicadas(texto){
+    //criando uma variável que recebe o texto do arquivo separado por espaços
+    const listaPalavras = texto.split(' ');
+    const resultado = {};
+    //objeto[propriedade] = valor;
+    listaPalavras.forEach(palavra => {
+        //verifica se a palavra tem mais de 3 caracteres
+        if(palavra.length >=3){
+        const palavraLimpa = limpaPalavras(palavra);
+        //se a palavra já existir no objeto, incrementa 1 no valor, se não, cria a propriedade com o valor 1
+        resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+        }
+    });
+    return resultado;
+}
+//função que quebra o texto em parágrafos
+function quebraEmParagrafos(texto){
+    const paragrafos = texto.toLowerCase().split('\n');
+    const contagem = paragrafos.flatMap(paragrafo => {
+        if(!paragrafo) return [];
+        return verificarPalavrasDuplicadas(paragrafo);
+    });
+    console.log(contagem);
+}
+//função que limpa as palavras de caracteres especiais
+function limpaPalavras(palavra){
+    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+}
